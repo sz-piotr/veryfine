5698334e7aa632e38a0648b05fe3fca3697ce5b1
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-12-25 22:10:12 +0100

    Add stack trace manipulation test

--- test/AssertionError.ts
+++ test/AssertionError.ts
@@ -1,22 +1,49 @@
-import { expect } from 'chai'
+import { expect as doExpect } from 'chai'
+import { expect } from '../src'
 import { AssertionError } from '../src/AssertionError'
 
 describe('AssertionError', () => {
   it('can be constructed', () => {
     const error = new AssertionError('message', 'expected', 'actual')
-    expect(error.name).to.equal('AssertionError')
-    expect(error.message).to.equal('message')
-    expect(error.expected).to.equal('expected')
-    expect(error.actual).to.equal('actual')
-    expect(error instanceof AssertionError).to.equal(true)
+    doExpect(error.name).to.equal('AssertionError')
+    doExpect(error.message).to.equal('message')
+    doExpect(error.expected).to.equal('expected')
+    doExpect(error.actual).to.equal('actual')
+    doExpect(error instanceof AssertionError).to.equal(true)
   })
 
   it('can be constructed with only one parameter', () => {
     const error = new AssertionError('message')
-    expect(error.name).to.equal('AssertionError')
-    expect(error.message).to.equal('message')
-    expect(error.expected).to.equal(undefined)
-    expect(error.actual).to.equal(undefined)
-    expect(error instanceof AssertionError).to.equal(true)
+    doExpect(error.name).to.equal('AssertionError')
+    doExpect(error.message).to.equal('message')
+    doExpect(error.expected).to.equal(undefined)
+    doExpect(error.actual).to.equal(undefined)
+    doExpect(error instanceof AssertionError).to.equal(true)
+  })
+
+  it('manipulates the stack trace for custom validators', () => {
+    let error
+    try {
+      expect(1).toSatisfy(() => ({
+        success: false,
+        message: 'multiline\nmessage',
+        negatedMessage: ''
+      }))
+    } catch (e) {
+      error = e
+    }
+    doExpect(error.stack)
+      .to.match(/^AssertionError: multiline\nmessage\n\s+at Expect.toSatisfy/)
+  })
+
+  it('manipulates the stack trace for built in validators', () => {
+    let error
+    try {
+      expect(1).toStrictlyEqual(2)
+    } catch (e) {
+      error = e
+    }
+    doExpect(error.stack).to.match(/at Expect.toStrictlyEqual/)
+    doExpect(error.stack).not.to.match(/at Expect.toSatisfy/)
   })
 })
