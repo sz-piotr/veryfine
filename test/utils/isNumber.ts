107e51cbb3b8a2fbd1d79ae64668ac109c91f274
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2018-12-26 20:39:20 +0100

    Add isNumber tests

---
+++ test/utils/isNumber.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai'
+import { isNumber } from '../../src/utils/isNumber'
+
+describe('isNumber', () => {
+  it('returns true for numbers', () => {
+    expect(isNumber(1)).to.equal(true)
+    expect(isNumber(-1)).to.equal(true)
+    expect(isNumber(0)).to.equal(true)
+    expect(isNumber(Infinity)).to.equal(true)
+    expect(isNumber(-Infinity)).to.equal(true)
+    expect(isNumber(3.14)).to.equal(true)
+  })
+
+  it('returns false for non-numbers and NaN', () => {
+    expect(isNumber('string')).to.equal(false)
+    expect(isNumber(true)).to.equal(false)
+    expect(isNumber([])).to.equal(false)
+    expect(isNumber({})).to.equal(false)
+    expect(isNumber(new Number())).to.equal(false)
+    expect(isNumber(NaN)).to.equal(false)
+  })
+})
